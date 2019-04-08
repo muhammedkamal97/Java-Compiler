92bb7018a093586fd3af6900608480469f24d5ef
Khaled Alkiek <k.alkiek@gmail.com>
2019-04-08 19:50:55 +0200

    Implement configuration interepreter.

--- src/lexical/LexicalConfigInterpreter.cpp
+++ src/lexical/LexicalConfigInterpreter.cpp
@@ -3,3 +3,124 @@
 //
 
 #include "LexicalConfigInterpreter.h"
+#include <fstream>
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/tokenizer.hpp>
+#include <regex>
+
+typedef boost::tokenizer<boost::char_separator<char>>tokenizer;
+
+const char* LexicalConfigInterpreter::secondary_delimiters = "-[]^$|?*+()\\";
+const regex LexicalConfigInterpreter::punctuation_regex("^\\[.*\\]$");
+const regex LexicalConfigInterpreter::keywords_regex("^\\{.*\\}$");
+const regex LexicalConfigInterpreter::regdef_regex("^\\w+ = .*$");
+const regex LexicalConfigInterpreter::regex_regex("^\\w+: .*$");
+
+LexicalConfigInterpreter::LexicalConfigInterpreter(fstream *config, bool cacheable) : config(config) {
+    punctuations = new vector<string>;
+    keywords = new vector<string>;
+    regdefs = new vector<string>;
+    regdef_map = new map<string, vector<string>>;
+    regexes = new vector<string>;
+    regex_map = new map<string, vector<string>>;
+}
+
+void LexicalConfigInterpreter::generate_rules_map() {
+
+    string line;
+    while(getline(*config, line)) {
+        if (line.length() <= 0) continue;
+
+        if (regex_match(line, punctuation_regex))
+            parse_punctations(line);
+        else if (regex_match(line, keywords_regex))
+            parse_keywords(line);
+        else if (regex_match(line, regdef_regex))
+            parse_regdef(line);
+        else if (regex_match(line, regex_regex))
+            parse_regex(line);
+        else {
+            // TODO: Handle invalid line
+        }
+    }
+}
+
+void LexicalConfigInterpreter::parse_punctations(string line) {
+    line = line.substr(1, line.length() - 2);       // Remove surrounding curly braces
+
+    vector<string> v;
+    boost::char_separator<char> sep;
+    tokenizer tokens(line, sep);
+    for (tokenizer::iterator tok_iter = tokens.begin(); tok_iter != tokens.end(); ++tok_iter) {
+        string token = *tok_iter;
+        if (*tok_iter == "\\") {    // Merge with next character
+            tok_iter++;
+            token = "\\" + *tok_iter;
+        }
+        punctuations->push_back(token);
+    }
+}
+
+void LexicalConfigInterpreter::parse_keywords(string line) {
+    line = line.substr(1, line.length() - 2);       // Remove surrounding brackets
+
+    vector<string> v;
+    boost::char_separator<char> sep;
+    tokenizer tokens(line, sep);
+    for (tokenizer::iterator tok_iter = tokens.begin(); tok_iter != tokens.end(); ++tok_iter) {
+        string token = *tok_iter;
+        if (*tok_iter == "\\") {    // Merge with next character
+            tok_iter++;
+            token = "\\" + *tok_iter;
+        }
+        keywords->push_back(token);
+    }
+}
+
+void LexicalConfigInterpreter::parse_regdef(string line) {
+    size_t equal_pos = line.find("="); //find location of word
+    string name = line.substr(0, equal_pos);
+    boost::algorithm::trim(name);
+
+    line.erase(0,equal_pos+1); //delete everything prior to location found
+    boost::algorithm::trim(name);
+
+    vector<string> tokens_vector;
+    boost::char_separator<char> sep(" ", secondary_delimiters);
+    tokenizer tokens(line, sep);
+    for (tokenizer::iterator tok_iter = tokens.begin(); tok_iter != tokens.end(); ++tok_iter) {
+        string token = *tok_iter;
+        if (*tok_iter == "\\") {    // Merge with next character
+            tok_iter++;
+            token = "\\" + *tok_iter;
+        }
+        tokens_vector.push_back(token);
+    }
+    regdefs->push_back(name);
+    regdef_map->insert(pair<string, vector<string>>(name, tokens_vector));
+}
+
+
+void LexicalConfigInterpreter::parse_regex(string line) {
+    size_t equal_pos = line.find(":"); //find location of word
+    string name = line.substr(0, equal_pos);
+    boost::algorithm::trim(name);
+
+    line.erase(0,equal_pos+1); //delete everything prior to location found
+    boost::algorithm::trim(name);
+
+    vector<string> tokens_vector;
+    boost::char_separator<char> sep(" ", secondary_delimiters);
+    tokenizer tokens(line, sep);
+    for (tokenizer::iterator tok_iter = tokens.begin(); tok_iter != tokens.end(); ++tok_iter) {
+        string token = *tok_iter;
+        if (*tok_iter == "\\") {    // Merge backslash with the espcaed character
+            tok_iter++;
+            token = "\\" + *tok_iter;
+        }
+        tokens_vector.push_back(token);
+    }
+    regexes->push_back(name);
+    regex_map->insert(pair<string, vector<string>>(name, tokens_vector));
+}
--- src/lexical/LexicalConfigInterpreter.h
+++ src/lexical/LexicalConfigInterpreter.h
@@ -8,18 +8,46 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <regex>
+#include <vector>
 
 using namespace std;
 
 class LexicalConfigInterpreter {
 private:
     map<string, vector<string>> *lexical_rules;
+    fstream *config;
+
+    vector<string>* punctuations;
+    vector<string>* keywords;
+    vector<string>* regdefs;
+    // Maps regular definition by its name in regdefs to its sequence of symbols
+    map<string, vector<string>>* regdef_map;
+    vector<string>* regexes;
+    // Maps regular expression by its token name in regexes to its sequence of symbols
+    map<string, vector<string>>* regex_map;
+
+    static const char* secondary_delimiters;
+    static const regex punctuation_regex;
+    static const regex keywords_regex;
+    static const regex regdef_regex;
+    static const regex regex_regex;
+
+    void parse_punctations(string line);
+    void parse_keywords(string line);
+    void parse_regdef(string line);
+    void parse_regex(string line);
 public:
     LexicalConfigInterpreter(fstream *config, bool cacheable = false);
 
     void generate_rules_map();
 
-    map<string, vector<string>> *get_rules();
+    vector<string>* get_punctations() { return punctuations; };
+    vector<string>* get_keywords() { return keywords; };
+    vector<string>* get_regdefs() { return regdefs; };
+    map<string, vector<string>>* get_regdef_map() { return regdef_map; };
+    vector<string>* get_regexes() { return regexes; };
+    map<string, vector<string>>* get_regex_map() { return regex_map; };
 
 };
 
