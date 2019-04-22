5d132b157b70775d80e607cf07804f142939bfc9
Khaled Alkiek <k.alkiek@gmail.com>
2019-04-22 23:10:11 +0200

    Implement follow sets generation.

--- src/parser/generator/Follow.h
+++ src/parser/generator/Follow.h
@@ -13,5 +13,11 @@
 
 using namespace std;
 
+typedef boost::adjacency_list < boost::vecS, boost::vecS, boost::directedS > Graph;
+typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
+typedef std::vector< Vertex > container;
+
 map<string, vector<string> *> *
-compute_follow(ProductionRules *productions);
+compute_follow(ProductionRules *productions, map<string, vector<string>*>* first_sets);
+Graph to_dag(Graph graph, int nodes_count, map<int, vector<int>*>* component_to_nodes);
+vector<int>* topological_sort(Graph graph);
